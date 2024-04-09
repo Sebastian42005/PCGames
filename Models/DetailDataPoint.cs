ff995a0ccaa8a903993e32c5a79d637324fc6420
Sebastian42005 <sebi4ederer@gmail.com>
2024-04-09 14:04:29 +0200

    changes

---
+++ Models/DetailDataPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.Serialization;
+
+namespace PCGamesFinal.Models
+{
+	[DataContract]
+	public class DetailDataPoint
+	{
+		public DetailDataPoint(double x, double y)
+		{
+			this.x = x;
+			this.Y = y;
+		}
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "x")]
+		public Nullable<double> x = null;
+
+		//Explicitly setting the name to be used while serializing to JSON.
+		[DataMember(Name = "y")]
+		public Nullable<double> Y = null;
+		public DateTime date { get; set; }
+	}
+}
